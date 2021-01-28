de4d04fbd3b4c30e80039f817dc0e0618ee0e5af
cccxxxppp <62333889+cccxxxppp@users.noreply.github.com>
2021-01-29 00:35:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,16 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.254/g' package/base-files/files/bin/config_generate
+sed -i '/set network.$1.netmask='$netm'/a\ \t\t\t\tset\ network.$1.dns='"'"'218.85.152.99\ 218.85.157.99'"'"'' package/base-files/files/bin/config_generate
+sed -i '/set network.$1.netmask='$netm'/a\ \t\t\t\tset\ network.$1.gateway='"'"'192.168.123.1'"'"'' package/base-files/files/bin/config_generate
+
+# Modify default Theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' feeds/luci/collections/luci/Makefile
+
+#cd package/lean/
+#rm -rf luci-theme-argon  
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  
+#git clone https://github.com/jerrykuku/lua-maxminddb.git
+#git clone https://github.com/jerrykuku/luci-app-vssr.git
+#git clone https://github.com/tty228/luci-app-serverchan.git
